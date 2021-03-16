05a848a3bcc361bdbde6c4a9e13a80b96daa3ed4
Ryan Waggoner <ryan@waggoner.co>
2021-03-16 02:28:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,6 +24,18 @@ def valid_move?(board, index)
     true
   elsif board[index] == "X" || board[index] == "O"
     false
+  elsif board[index] == nil
+    false
+def position_taken?(board, index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+    end
   end
 end
 
